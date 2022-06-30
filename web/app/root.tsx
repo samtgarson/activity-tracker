6769416d55b28c15e7c2dc3aad0bf1b2b75ec822
Sam Garson <samtgarson@gmail.com>
2022-06-30 19:24:51 +0100

    Setup tailwind

--- web/app/root.tsx
+++ web/app/root.tsx
@@ -8,12 +8,18 @@ import {
   Scripts,
   ScrollRestoration
 } from '@remix-run/react'
+import type { LinksFunction } from '@remix-run/node' // or "@remix-run/cloudflare"
+import styles from './styles/index.css'
 
 export const meta: MetaFunction = () => ({
   charset: 'utf-8',
   title: 'New Remix App',
   viewport: 'width=device-width,initial-scale=1'
 })
+
+export const links: LinksFunction = () => {
+  return [{ rel: 'stylesheet', href: styles }]
+}
 
 export default function App() {
   return (
--- web/remix.config.js
+++ web/remix.config.js
@@ -8,6 +8,5 @@ module.exports = {
   ignoredRouteFiles: ['**/.*']
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "api/index.js",
   // publicPath: "/build/",
 }
