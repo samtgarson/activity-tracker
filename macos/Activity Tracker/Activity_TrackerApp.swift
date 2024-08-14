1de3d101fbd81329b5300600749778c3fba50624
Sam Garson <samtgarson@gmail.com>
2024-08-14 10:53:06 +0100

    Instantiate macOS app and show menubar extra

---
+++ macos/Activity Tracker/Activity_TrackerApp.swift
@@ -0,0 +1,31 @@
+//
+//  Activity_TrackerApp.swift
+//  Activity Tracker
+//
+//  Created by Samuel Garson on 13/08/2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Activity_TrackerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            User.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        MenuBarExtra("ActivityTracker", systemImage: "hammer") {
+            AppMenu()
+        }.modelContainer(sharedModelContainer)
+    }
+}
---
+++ macos/Activity Tracker/AppMenu.swift
@@ -0,0 +1,22 @@
+//
+//  AppMenu.swift
+//  Activity Tracker
+//
+//  Created by Samuel Garson on 13/08/2024.
+//
+
+import SwiftUI
+
+struct AppMenu: View {
+    var body: some View {
+        Text("No events today")
+            
+        Divider()
+
+        Button("Quit Activity Tracker completely") { NSApplication.shared.terminate(nil) }
+    }
+}
+
+#Preview {
+    AppMenu()
+}
